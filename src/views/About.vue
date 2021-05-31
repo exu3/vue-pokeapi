aade1956b90c8fbbd0037c877602dcdabc246b89
eilla1 <git@ella.cx>
2021-05-31 01:34:11 -0700

    :rocket: get the search indexing thing to work

--- src/views/About.vue
+++ src/views/About.vue
@@ -1,6 +1,40 @@
 <template>
   <div class="about">
-    <h1>This is an about lol test page</h1>
-    <h3>{{$route.params.slug}}</h3>
-   </div>
+    <div
+      v-if="pokemon"
+      class="w-1/4 m-auto shadow-2xl flex justify-center flex-col items-center p-4 rounded-xl"
+    >
+      <h3 class="text-2xl text-green-900 uppcase">{{ pokemon.name }}</h3>
+      <div class="flex justify-center">
+        <img class="w-48" :src="pokemon.sprites.front_shiny" alt="" />
+        <img class="w-48" :src="pokemon.sprites.back_shiny" alt="" />
+      </div>
+      <h3 class="text-yellow-400">Types</h3>
+      <div v-for="(type, idx) in pokemon.types" :key="idx">
+        <h5 class="text-blue-900">{{ type.type.name }}</h5>
+      </div>
+    </div>
+  </div>
 </template>
+
+<script>
+import { useRoute } from "vue-router";
+import { reactive, toRefs } from "vue";
+export default {
+  setup() {
+    const route = useRoute();
+    const state = reactive({
+      pokemon: null,
+    });
+
+    fetch(`https://pokeapi.co/api/v2/pokemon/${route.params.slug}`)
+      .then((res) => res.json())
+      .then((data) => {
+        console.log(data);
+        state.pokemon = data;
+      });
+
+    return { ...toRefs(state) };
+  },
+};
+</script>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,20 +1,59 @@
 <template>
-  <div class="home">
-    <h3>hello world</h3>
+  <div class="w-full flex justify-center">
+    <input
+      type="text"
+      placeholder="Search pokemon"
+      class="mt-10 p-2 border-blue-200 border-2"
+      v-model="text"
+    />
+  </div>
+  <div class="mt-10 p-4 flex flex-wrap justify-center">
+    <div
+      class="ml-4 text-2xl text-blue-300"
+      v-for="(pokemon, idx) in filteredPokemon"
+      :key="idx"
+    >
+      <router-link :to="`/about/${urlIdLookup[pokemon.name]}`">
+        {{ pokemon.name }}
+      </router-link>
+    </div>
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
+import { reactive, toRefs, computed } from "vue";
 
 export default {
   name: "Home",
-  setup(){
+  setup() {
+    const state = reactive({
+      pokemons: [],
+      urlIdLookup: {},
+      text: "",
+      filteredPokemon: computed(() => updatePokemon()),
+    });
+
+    function updatePokemon() {
+      if (!state.text) {
+        return [];
+      }
+      return state.pokemons.filter((pokemon) =>
+        pokemon.name.includes(state.text)
+      );
+    }
+
     fetch("https://pokeapi.co/api/v2/pokemon?offset=0")
-    .then((res)=> res.json())
-    .then((data)=> {
-      console.log(data)
-    } )
-  }
+      .then((res) => res.json())
+      .then((data) => {
+        console.log(data);
+        state.pokemons = data.results;
+        state.urlIdLookup = data.results.reduce(
+          (acc, cur, idx) => (acc = { ...acc, [cur.name]: idx + 1 }),
+          {}
+        );
+      });
+    return { ...toRefs(state) };
+  },
 };
 </script>
